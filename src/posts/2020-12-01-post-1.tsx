d702279228268187be6b2a490c857bf020902bb8
elshafie <elshafie.eltayeb@gmail.com>
2021-11-04 22:42:57 +0100

    initial commit

---
+++ src/posts/2020-12-01-post-1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Post1: React.FC = (props) => {
+    return (
+        <header className='relative z-10 w-screen mx-auto'>
+            <div className='relative bg-white mx-auto'>
+                <h1>post 1</h1>
+            </div>
+        </header>
+    );
+};
