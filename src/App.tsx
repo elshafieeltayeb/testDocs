59c4e7036bebe441bbddc5ee04ac2281d467b96e
elshafie <elshafie.eltayeb@gmail.com>
2021-11-04 22:47:19 +0100

    _

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function Home() {
         <>
             <main>
                 <h2>Welcome to the homepage!</h2>
-                <p>You can do this, I believe in you.</p>
+                <p>You can do this, I believe in you test.</p>
             </main>
             <nav>
                 <Link to='/about'>About</Link>
